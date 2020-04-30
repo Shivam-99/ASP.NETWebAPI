4007ab0c21359d3cdc9d2574e2a6e63fa84993ce
Shivam-99 <37024449+Shivam-99@users.noreply.github.com>
2020-04-30 17:54:49 +0530

    Adding Project Files

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        [MaxLength(256)]
+        public string Address { get; set; }
+    }
+}
